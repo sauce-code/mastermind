937996601bd6ce9169d63fc298fa6f9044d3c18d
Torben <mail@sauce-code.com>
2017-01-05 14:00:08 +0100

    implementiere speicherenaechstenVersuch(int)

--- src/model/Mastermind.java
+++ src/model/Mastermind.java
@@ -1,4 +1,5 @@
 package model;
+
 /**
  * Mastermind-Spiel.
  * 
@@ -21,27 +22,32 @@ public class Mastermind {
 	 * Maximale Anzahl Versuche pro Partie.
 	 */
 	public static final int MAX_VERSUCHE = 10;
-	
+
 	/**
 	 * Speichert alle Versuche ab.
 	 */
-	private int[][] versuche;
-	
+	private int[] versuche;
+
 	/**
 	 * Speichert die Nummer des aktuellen Versuchs ab.
 	 */
 	private int aktuellerVersuch;
-	
+
 	/**
 	 * Die L&ouml;sung des Spiels.
 	 */
 	private int loesung;
 
 	/**
+	 * Die CodeL&auml;nge jedes Versuchs.
+	 */
+	private int codeLaenge;
+
+	/**
 	 * Die Anzahl der zugelassenen Farben.
 	 */
 	private int farbAnzahl;
-	
+
 	/**
 	 * Unzul&auml;ssige Angaben werden kommentarlos durch die Standardwerte des
 	 * Originalspiels {@link #Mastermind()} ersetzt.
@@ -50,7 +56,8 @@ public class Mastermind {
 	 * @param farbAnzahl
 	 */
 	public Mastermind(int codeLaenge, int farbAnzahl) {
-		
+		this.farbAnzahl = farbAnzahl;
+		this.versuche = new int[MAX_VERSUCHE];
 	}
 
 	/**
@@ -73,6 +80,24 @@ public class Mastermind {
 	 * @return {@code true}, falls Versuch g&uuml;ltig
 	 */
 	public boolean speichereNaechstenVersuch(int versuch) {
+		if (!versuchInOrdnung(versuch)) {
+			return false;
+		}
+		versuche[aktuellerVersuch] = versuch;
+		return true;
+	}
+
+	/**
+	 * Gibt an, ob ein Versuch in Ordnung ist.<br>
+	 * Er ist genau dann in Ordnung, wenn seine Codel&auml;nge und Farbanzahl
+	 * keine Limits &uuml;berschreiten.
+	 * 
+	 * @param versuch
+	 *            Versuch, welcher &uuml;berpr&uuml;ft werden soll
+	 * @return {@code true}, falls Veruch in ordnung
+	 */
+	private boolean versuchInOrdnung(int versuch) {
+		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -91,7 +116,8 @@ public class Mastermind {
 	 * mit den Nummern 1 bis 7.
 	 * 
 	 * @param index
-	 * @return <ul>
+	 * @return
+	 * 		<ul>
 	 *         <li>{@code -1}, falls Index ung&uuml;ltig</li>
 	 *         <li>{@code index}–te Versuch, sonst</li>
 	 *         </ul>
@@ -111,7 +137,8 @@ public class Mastermind {
 	 * 
 	 * @param versuch
 	 *            welcher Versuch?
-	 * @return <ul>
+	 * @return
+	 * 		<ul>
 	 *         <li>{@code -1}, falls Index ung&uuml;ltig</li>
 	 *         <li>Anzahl {@code s} der schwarzen und die Anzahl {@code w} der
 	 *         wei&szlig;en Stifte f&uuml;r den &uuml;bergebenen Versuch als
@@ -125,9 +152,10 @@ public class Mastermind {
 	/**
 	 * Pr&uuml;ft, ob der zuletzt abgegebene Versuch er- folgreich war.
 	 * 
-	 * @return <ul>
-	 *         <li>{@ode true}, falls der zuletzt abgegebene Versuch
-	 *         erfolgreich war</li>
+	 * @return
+	 * 		<ul>
+	 *         <li>{@ode true}, falls der zuletzt abgegebene Versuch erfolgreich
+	 *         war</li>
 	 *         <li>{@code false}, falls der zuletzt abgegebene Versuche nicht
 	 *         erfolgreich war</li>
 	 *         <li>{@code false}, falls noch gar keine Versuche gab, weil das
@@ -143,7 +171,7 @@ public class Mastermind {
 	 * werden gel&ouml;scht und es wird ein neuer Zufallscode erzeugt.
 	 */
 	public void spielZuruecksetzen() {
-		
+
 	}
 
 }
