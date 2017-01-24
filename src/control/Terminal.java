d72553fa3d2877866e6d6a3fe0e027fa1df2e460
Torben <mail@sauce-code.com>
2017-01-24 18:32:00 +0100

    javadoc

--- src/control/Terminal.java
+++ src/control/Terminal.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 /**
- * Hilfs-Klasse zum Einlesen von Zahlen von der Konsole.
+ * Hilfsklasse zum Einlesen von Zahlen von der Konsole.
  * 
  * @author ich
  *
